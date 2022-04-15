ba78eac64f05affaacbcd94d7faafba35b5cd363
Saad-iit <77770818+Saad-iit@users.noreply.github.com>
2022-04-15 02:40:21 -0500

    Initial Commit
    API Keys

---
+++ config.py
@@ -0,0 +1,11 @@
+#API keys used by main
+
+consumer_key = "YOUR_CONSUMER_KEY"
+
+consumer_secret = "YOUR_CONSUMER_SECRET"
+
+access_key= "YOUR_ACCESS_KEY"
+
+access_secret = "YOUR_ACCESS_SECRET"
+
+API_KEY = "YOUR_YOUTUBE_API_KEY"
