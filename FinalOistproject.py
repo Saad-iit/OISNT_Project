db1312c1dd9eafed357792a5ddf44588a554496c
Saad-iit <77770818+Saad-iit@users.noreply.github.com>
2022-04-19 02:39:45 -0500

    better gui and error catch

--- FinalOistproject.py
+++ FinalOistproject.py
@@ -8,22 +8,25 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas as pd
 import dfgui
 import csv
-import tkinter as tk
 from tkinter import *
-from tkinter import messagebox
-from tkinter import simpledialog
+from functools import partial
 
-def twitter():
+   
+def twitter(search):
+    global errorlabel
+    if errorlabel:
+        errorlabel.destroy()
+    search = search.get()
     #api keys provided by twitter
     consumer_key = config.consumer_key
     consumer_secret = config.consumer_secret
     access_key= config.access_key
     access_secret = config.access_secret
-    tweet_num = 5 #change number of tweets grabbed
+    tweet_num = 5
     tweets = []
+    data = []
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
-     
     api = tweepy.API(auth,wait_on_rate_limit=True)
     #getting results from search  
     results = tweepy.Cursor(api.search_tweets,q=search+ " -filter:retweets",
@@ -37,15 +40,22 @@ def twitter():
             'Time': status.created_at,
             'Nr of retweets': status.retweet_count,
             'Nr of favorited': status.favorite_count}
-        tweets.append(data) 
-    #coverting that data into a csv and gui
-    twitinfo = pd.DataFrame(tweets)  
-    twitinfo.head()
-    twitinfo.to_csv('twitter.csv', encoding='utf-8')
-    print (twitinfo)
-    dfgui.show(twitinfo)
-
-def youtube(): 
+        tweets.append(data)
+    if len(data):
+        #coverting that data into a csv and gui
+        twitinfo = pd.DataFrame(tweets)  
+        twitinfo.head()
+        twitinfo.to_csv('twitter.csv', encoding='utf-8')
+        print (twitinfo)
+        dfgui.show(twitinfo)
+    else:
+        errorlabel = Label(tk, text="No data available for search, please retry")
+        errorlabel.place(x=160, y=430)
+def youtube(search):
+    global errorlabel
+    if errorlabel:
+        errorlabel.destroy()
+    search = search.get()
     rows = []
     videoID = []
     SCOPES = 'https://www.googleapis.com/auth/youtube.force-ssl'
@@ -59,8 +69,8 @@ def youtube():
             part="id,snippet",
             type='video',
             q=search,
-            videoDuration='short',
-            videoDefinition='high',
+            videoDuration='any',
+            videoDefinition='any',
             maxResults=1,
             fields="nextPageToken,items(id(videoId),snippet(publishedAt,channelId,channelTitle,title,description))"
     )
@@ -68,40 +78,44 @@ def youtube():
     #for each video found looks through comments
     for items in response['items']:
         videoID.append(items['id']['videoId'])
-        for item in videoID:
-            video_response=youtube.commentThreads().list(
-            part='id,snippet,replies',
-            videoId=item,
-            maxResults=6
-            ).execute()
-            #grabs the comments and places them in list
-            for item in video_response['items']:
-                comment = str(item['snippet']['topLevelComment']['snippet']['textDisplay'])
-                rows.append([
-                        items['snippet']['channelTitle'],
-                        items['snippet']['title'],
-                        items['snippet']['description'],comment])
-
-    #coverts into an csv and gui
-    ytinfo = pd.DataFrame(rows, columns = ["Channel Name", "Title", "Description", "Comment" ])
-    ytinfo.to_csv('youtube.csv', encoding='utf-8')
-    print(ytinfo)
-    dfgui.show(ytinfo)
-
-#initial search
-root = tk.Tk()
-root.withdraw()
-search = simpledialog.askstring("Database Search", "Would what you like to learn about?")
-
-    
-#makes sure input is not blank or whitespace
-if search and not search.isspace():
-    tk = Tk()
-    tk.geometry("600x500")
-    tk.title("Please Choose")
-    b1 = Button(tk, text="Twitter", command=twitter).place(x=100, y=100)
-    b2 = Button(tk, text="YouTube", command=youtube).place(x=300, y=100)
-    tk.mainloop()
+        try:
+            for item in videoID:
+                video_response=youtube.commentThreads().list(
+                part='id,snippet,replies',
+                videoId=item,
+                maxResults=6
+                ).execute()
+                #grabs the comments and places them in list
+                for item in video_response['items']:
+                    comment = str(item['snippet']['topLevelComment']['snippet']['textDisplay'])
+                    rows.append([
+                            items['snippet']['channelTitle'],
+                            items['snippet']['title'],
+                            items['snippet']['description'],comment])
+        except:
+            print("Video has comments disabled")
+            
+    if len(rows):
+        #coverts into an csv and gui
+        ytinfo = pd.DataFrame(rows, columns = ["Channel Name", "Title", "Description", "Comment" ])
+        ytinfo.to_csv('youtube.csv', encoding='utf-8')
+        print(ytinfo)
+        dfgui.show(ytinfo)
+    else:
+        errorlabel = Label(tk, text="No data available for search, please retry")
+        errorlabel.place(x=160, y=430)
         
-
-
+    
+#building gui
+tk = Tk()
+tk.geometry("600x500")
+tk.title("Please Choose")
+searchLabel = Label(tk, text="Search for Data:").place(x=140, y=150)
+search = StringVar()
+errorlabel = None
+searchEntry = Entry(tk, textvariable=search).place(x=230, y=150)
+youtube = partial(youtube, search)
+twitter = partial(twitter, search)
+b1 = Button(tk, text="On Twitter", command=twitter).place(x=180, y=200)
+b2 = Button(tk, text="On YouTube", command=youtube).place(x=280, y=200)
+tk.mainloop()     
